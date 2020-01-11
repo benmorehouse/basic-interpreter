c1900afd0127eb7bcd7717f412c5adaf55dc8eae
Benjamin Morehouse <bmorehou@usc.edu>
2020-01-10 22:08:48 -0800

    updates

---
+++ frontend/basic-interpreter/src/components/frame/frame.js
@@ -0,0 +1,16 @@
+import { Component, h, Prop } from '@stencil/core';
+
+@Component({
+	tag: 'main-frame',
+	styleUrl: 'frame.css',
+})
+
+export class frame{
+	render(){
+		return(
+			<div class="top-bar">
+					
+			</div>
+		)
+	}
+}
