04ed0d34712d5c034b9a654a9f80753de96a6ba3
Benjamin Morehouse <bmorehou@usc.edu>
2020-01-12 16:03:05 -0800

    Updates

--- frontend/basic-interpreter/src/components/terminal/terminal.tsx
+++ frontend/basic-interpreter/src/components/terminal/terminal.tsx
@@ -1,16 +1,15 @@
 import { Component, Host, h } from '@stencil/core';
 import '../../../node_modules/xterm/lib/xterm.js';
+import {Terminal} from 'xterm';
 
 @Component({
-  tag: 'main-terminal',
-  styleUrl: '../../../node_modules/xterm/css/xterm.css',
-  shadow: true,
+	tag: 'main-terminal',
+	styleUrl: '../../../node_modules/xterm/css/xterm.css',
+	shadow: true,
 })
+
 export class Terminal {
-	var term = new Terminal();
-	term.open(document.getElementById('terminal'));
-	term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
-   
+
  render() {
     return (
 		<Host>
@@ -22,5 +21,4 @@ export class Terminal {
 
     );
   }
-
 }
--- frontend/basic-interpreter/src/helpers/utils.ts
+++ frontend/basic-interpreter/src/helpers/utils.ts
@@ -1,5 +1,13 @@
-
+import "node_modules/xterm/lib/xterm.js";
 
 export function sayHello() {
   return Math.random() < 0.5 ? 'Hello' : 'Hola';
 }
+
+export function terminalWriter(){
+	var term = new Terminal();
+	term.open(document.getElementById('terminal'));
+	term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ')
+}
+   
+
