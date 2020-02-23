fa3ed7950b3baf189b79836306b40f10e0cf0212
Benjamin Morehouse <bmorehou@usc.edu>
2020-02-23 11:00:32 -0800

    Added a roadmap as well as errorHandling started

--- backend/operatingSystem/main.cpp
+++ backend/operatingSystem/main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <vector>
-#include "arithmetic.h"
-#include "boolean.h"
-#include "command.h"
-#include "interpreter.h"
-#include "compiler.h"
 
-int main(int argc, char* argv[]){
-	if(argc < 2){ std::cout<<"Please enter in a text file that the interpreter can operate"<<std::endl;}
-	std::ifstream input;
-	input.open(argv[1]);
-	
-	if(input.fail() == true){
-		std::cout<<"Please enter in a file that the interpreter can use"<<std::endl;
-	}
-
-	Interpreter interpreter(input);
-	Compiler compiler(interpreter.getOutFile(),"compilerOutput.txt",&interpreter);
-	compiler.main();
+class errorHandler {
+	public:
+		errorHandler();
+		~errorHandler();
+		void Err(string err);
+	private: 
+		void handleError(string err);
 }
 
+errorHandler::errorHandler() {}
+
+errorHandler::~errorHandler() {}
+
+void errorHandler::Err(string err) {
+	this.handleError(err);
+}
+
+void handleError(string err) {
+	std::cout<<"ERROR : "<<err<<std::endl;
+	return
+}
+
+
+int main(int argc, char **argv) {
+	
+	if (argc < 2) {
+		
+	}
+}
